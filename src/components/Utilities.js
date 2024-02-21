93cda9259a8d594b02da0f6558e42dbeabd9d702
Jobad <gen36.33@gmail.com>
2024-02-21 10:55:29 -0600

    Finanzas api enabled

--- src/components/Utilities.js
+++ src/components/Utilities.js
@@ -1,4 +1,4 @@
-// export const API_URL = 'https://finanzas.visssible.com/backend';
-export const API_URL = 'https://financedemo.visssible.com/backend';
+export const API_URL = 'https://finanzas.visssible.com/backend';
+// export const API_URL = 'https://financedemo.visssible.com/backend';
 
 
