ccaf2768e4ab0de2d261ccf3fbcb6e66d2369550
Jobad <gen36.33@gmail.com>
2024-09-09 11:18:30 -0500

    dreamhost api

--- src/components/Utilities.js
+++ src/components/Utilities.js
@@ -1,4 +1,5 @@
 // export const API_URL = 'https://finance.visssible.com/backend';
 // export const API_URL = 'https://financedemo.visssible.com/backend';
-export const API_URL = 'https://leonsayas.com/backend/';
-// export const API_URL = 'https://jobadsayas.com/financeBackend/';
+// export const API_URL = 'https://leonsayas.com/backend/';
+// export const API_URL = 'http://visssible.atwebpages.com/financeBackend/';
+export const API_URL = 'https://jobadsayas.com/financeBackend/';
