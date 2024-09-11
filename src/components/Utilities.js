dd951b55d4516c9623d0d3758af19041ad067907
Jobad <gen36.33@gmail.com>
2024-09-11 03:40:46 -0500

    Enabled jobsay.dreamhost api

--- src/components/Utilities.js
+++ src/components/Utilities.js
@@ -2,4 +2,4 @@
 // export const API_URL = 'https://financedemo.visssible.com/backend';
 // export const API_URL = 'https://leonsayas.com/backend/';
 // export const API_URL = 'http://visssible.atwebpages.com/financeBackend/';
-// export const API_URL = 'https://jobsay.dreamhosters.com/financeBackend/';
+export const API_URL = 'https://jobsay.dreamhosters.com/financeBackend/';
