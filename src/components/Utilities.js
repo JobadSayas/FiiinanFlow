6c2395b146b98d22b1fa08628a4dc9e0ebb15754
Jobad <gen36.33@gmail.com>
2024-09-12 09:58:45 -0500

    API hostinger areli

--- src/components/Utilities.js
+++ src/components/Utilities.js
@@ -1,5 +1,12 @@
-// export const API_URL = 'https://finance.visssible.com/backend';
-// export const API_URL = 'https://financedemo.visssible.com/backend';
+
+// En awardspace
 // export const API_URL = 'https://leonsayas.com/backend/';
-// export const API_URL = 'http://visssible.atwebpages.com/financeBackend/';
-export const API_URL = 'https://jobsay.dreamhosters.com/financeBackend/';
+
+// En awardspace financedemo
+// export const API_URL = 'https://financedemo.visssible.com/backend';
+
+// En dreamhost
+// export const API_URL = 'https://jobsay.dreamhosters.com/financeBackend/';
+
+// En hostinger Areli
+export const API_URL = 'https://lawngreen-oyster-299671.hostingersite.com/financeBackend';
