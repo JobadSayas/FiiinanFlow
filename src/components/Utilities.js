34f04278b75df76505c251400af01f1af916d7a4
Jobad <gen36.33@gmail.com>
2024-09-19 11:17:30 -0500

    API a mi hostinger server

--- src/components/Utilities.js
+++ src/components/Utilities.js
@@ -9,4 +9,10 @@
 // export const API_URL = 'https://jobsay.dreamhosters.com/financeBackend/';
 
 // En hostinger Areli
-export const API_URL = 'https://lawngreen-oyster-299671.hostingersite.com/financeBackend';
+// export const API_URL = 'https://lawngreen-oyster-299671.hostingersite.com/financeBackend';
+
+// En hostinger Vissible Finance
+export const API_URL = 'https://finance.visssible.com/backend/';
+
+// En hostinger Vissible Demo
+// export const API_URL = 'https://financedemo.visssible.com/backend/';
